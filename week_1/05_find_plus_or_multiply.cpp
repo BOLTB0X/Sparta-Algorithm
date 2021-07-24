3f60d2880f7ba182356be305fb572e28ec072d2a
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-07-24 16:10:28 +0900

    01주차_C++

--- week_1/05_find_plus_or_multiply.cpp
+++ week_1/05_find_plus_or_multiply.cpp
@@ -3,10 +3,11 @@ using namespace std;
 
 int find_max_plus_or_multiply(int* arr,int len) {
 	int multiply_sum = 0;
+	//매개변수인 arr를 하나씩 순회
 	for (int i = 0; i < len; i++) {
-		if (arr[i] <= 1 or multiply_sum <= 1)
+		if (arr[i] <= 1 or multiply_sum <= 1) // 현재 원소, 합이 1이하 일 경우
 			multiply_sum += arr[i];
-		else
+		else // 그 외
 			multiply_sum *= arr[i];
 	}
 	return multiply_sum;
