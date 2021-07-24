c674336400992f7570eeb4e7ed3dd99dafcaf695
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-07-24 16:07:02 +0900

    01주차_C++

--- week_1/04_is_number_exist.cpp
+++ week_1/04_is_number_exist.cpp
@@ -5,7 +5,7 @@ int arr[6] = { 3, 5, 6, 1, 2, 4 };
 
 bool is_number_exist(int number, int* arr) {
 	bool check=false;
-	for (int i = 0; i < 6; i++) {
+	for (int i = 0; i < 6; i++) { //하나씩 비교
 		if (arr[i] == number)
 			return true;
 	}
