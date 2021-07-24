94f22fe4b97bfc14ef735d5d2d2a5fbda915bd50
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-07-24 15:58:12 +0900

    01주차_C++

--- week_1/01_01_find_max_num.cpp
+++ week_1/01_01_find_max_num.cpp
@@ -6,7 +6,7 @@ int find_max_num(int* arr) {
 	int max_value=arr[0];
 	for (int i = 1; i < 5; i++) {
 		if (max_value < arr[i]) max_value=arr[i];
-	}
+	} //C++은 파이썬 처럼 if->else 가 조금 다름
 
 	return max_value;
 }
